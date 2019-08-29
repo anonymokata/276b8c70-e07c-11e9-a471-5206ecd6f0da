53dacf4baf303ac14374e74442e5f81be6232ba5
someone <someone>
2019-08-29 12:39:15 -0400

    beginning of program defining structure

---
+++ Pillar_Kata_Final_Grocery/MainProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pillar_Kata_Final_Grocery
+{
+    class MainProgram
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ Pillar_Kata_Final_Grocery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pillar_Kata_Final_Grocery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainProgram.Main();
+        }
+    }
+}
