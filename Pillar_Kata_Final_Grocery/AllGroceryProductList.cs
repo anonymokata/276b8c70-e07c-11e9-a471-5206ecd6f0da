13b0f14840e0f64eca7fa450669da2e9d3bc84b9
someone <someone>
2019-08-29 14:40:31 -0400

    created methos in class, test is now and methos id now refrensed

--- Pillar_Kata_Final_Grocery/AllGroceryProductList.cs
+++ Pillar_Kata_Final_Grocery/AllGroceryProductList.cs
@@ -4,5 +4,14 @@ using System.Text;
 
 namespace Pillar_Kata_Final_Grocery
 {
- 
+    class AllGroceryProductList
+    {
+        public static string[,] GroceryProduce()
+        {
+            string[,] produce = new string[,] { { "Lettuce", "1.29" }, { "Tomato", "1.00" }, { "Potaatos", ".35" }, };
+
+            return produce;
+        }
+
+    }
 }
