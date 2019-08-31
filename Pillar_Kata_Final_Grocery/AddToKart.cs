2d5957d1b7b578c60f6e351129c95cf95ea58d23
someone <someone>
2019-08-31 10:54:59 -0400

    still strugling with this user input into the two dimensional array....im not even sure on what data type to bring into the parameters. not sure on how to test either....i admit im cheating a little bt now by trying to figure out what i need to do to make this work. once i get this i will start over the right way and execute

--- Pillar_Kata_Final_Grocery/AddToKart.cs
+++ Pillar_Kata_Final_Grocery/AddToKart.cs
@@ -9,14 +9,14 @@ namespace Pillar_Kata_Final_Grocery
         
         public static string[,] AddToKart(string item,string price)
         {
-            string[,] kart = new string[,] { };
+            string[,] kart = new string[ ,] { { item, price} };
 
             for (int x = 0; x < kart.GetLength(0); x++)
             {
                 for (int y = 0; y < kart.GetLength(1); y++)
                 {
-                    kart[0, 0] ;
-                    kart[0, 1] ;
+                    kart[0, 0] = item ;
+                    kart[0, 1] = price;
                     return kart;
                 }
             }
